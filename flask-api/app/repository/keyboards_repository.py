4361c9ede9ba1c8d82f66249087a3b018da23d1c
Wesley-Costa <wesleycosta06@gmail.com>
2024-01-23 09:44:43 -0300

    feat: adição de repository e ajuste para autenticação

---
+++ flask-api/app/repository/keyboards_repository.py
@@ -0,0 +1,37 @@
+from ..models.keyboards import Keyboards, keyboard_schema, keyboards_schema
+from app import db
+
+
+def save(keyboard: Keyboards):
+    try:
+        db.session.add(keyboard)
+        db.session.commit()
+        return keyboard_schema.dump(keyboard)
+
+    except Exception as e:
+        db.session.rollback()
+        raise e
+
+
+def delete(keyboard: Keyboards):
+    try:
+        db.session.delete(keyboard)
+        db.session.commit()
+        return keyboard_schema.dump(keyboard)
+    except Exception as e:
+        db.session.rollback
+        raise e
+
+
+def update(keyboard: Keyboards, request):
+    try:
+        keyboard.brand = request["brand"]
+        keyboard.model = request["model"]
+        keyboard.color = request["color"]
+        keyboard.price = request["price"]
+
+        db.session.commit()
+        return keyboard_schema.dump(keyboard)
+    except Exception as e:
+        db.session.rollback()
+        raise e
--- flask-api/app/routes/routes.py
+++ flask-api/app/routes/routes.py
@@ -1,35 +1,38 @@
 import json
 from app import app
 from ..views import users, helper, keyboards
-from flask import jsonify, request
+from flask import jsonify
 
 
 @app.route("/", methods=["GET"])
 @helper.token_required
-def root():
+def root(current_user):
     return jsonify({"message": "Test message"})
 
 
-@app.route("/users/<id>", methods=["GET", "DELETE", "PUT"])
-def routes_by_user_specific(id):
-    if request.method == "GET":
-        return users.get_user(id)
-    elif request.method == "DELETE":
-        return users.delete_user(id)
-    elif request.method == "PUT":
-        return users.update_user(id)
-    else:
-        return jsonify({"message": "Página não encontrada", "data": {}}), 404
+@app.route("/users", methods=["POST"])
+def post_user():
+    return users.post_user()
 
 
-@app.route("/users", methods=["POST", "GET"])
-def routes_by_all_user():
-    if request.method == "POST":
-        return users.post_user()
-    elif request.method == "GET":
-        return users.get_users()
-    else:
-        return jsonify({"message": "Página não encontrada", "data": {}}), 404
+@app.route("/users/<id>", methods=["PUT"])
+def update_user(id):
+    return users.update_user(id)
+
+
+@app.route("/users", methods=["GET"])
+def get_users():
+    return users.get_users()
+
+
+@app.route("/users/<id>", methods=["GET"])
+def get_user(id):
+    return users.get_user(id)
+
+
+@app.route("/users/<id>", methods=["DELETE"])
+def delete_user(id):
+    return users.delete_user(id)
 
 
 @app.route("/auth", methods=["POST"])
@@ -37,23 +40,26 @@ def auth():
     return helper.auth()
 
 
-@app.route("/keyboards", methods=["POST", "GET"])
-def routes_by_keyboards():
-    if request.method == "POST":
-        return keyboards.post_keyboard()
-    elif request.method == "GET":
-        return keyboards.get_keyboards()
-    else:
-        return jsonify({"message": "Página não encontrada", "data": {}}), 404
+@app.route("/keyboards", methods=["GET"])
+def get_keyboards():
+    return keyboards.get_keyboards()
 
 
-@app.route("/keyboards/<id>", methods=["DELETE", "PUT", 'GET'])
-def routes_by_keyboard_specific(id):
-    if request.method == "DELETE":
-        return keyboards.delete_keyboard(id)
-    elif request.method == "PUT":
-        return keyboards.update_keyboard(id)
-    elif request.method == "GET":
-        return keyboards.get_keyboard(id)
-    else:
-        return jsonify({"message": "Página não encontrada", "data": {}}), 404
+@app.route("/keyboards/<id>", methods=["GET"])
+def get_keyboard(id):
+    return keyboards.get_keyboard(id)
+
+
+@app.route("/keyboards", methods=["POST"])
+def post_keyboards():
+    return keyboards.post_keyboard()
+
+
+@app.route("/keyboards/<id>", methods=["PUT"])
+def update_keyboards(id):
+    return keyboards.update_keyboard(id)
+
+
+@app.route("/keyboards/<id>", methods=["GET"])
+def delete_keyboards(id):
+    keyboards.delete_keyboard(id)
